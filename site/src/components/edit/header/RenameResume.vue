5547dbf5e1c6371466e59dbb34c70a3a0792dbe6
tiiiiiimmy <102719366+tiiiiiimmy@users.noreply.github.com>
2026-04-16 02:24:07 +1200

    ✨ feat: Show file resume names as read-only in the header
    - Prevent inline renaming for file-backed resumes in the editor header
    - Display the backing file name next to the current resume title

--- site/src/components/edit/header/RenameResume.vue
+++ site/src/components/edit/header/RenameResume.vue
@@ -1,10 +1,17 @@
 <template>
   <Editable
+    v-if="data.curResumeSource !== 'file'"
     id="edit-rename"
     class="w-100 mx-2 text-sm md:text-base"
     :default="data.curResumeName"
     :on-value-commit="rename"
   />
+  <div v-else class="w-100 mx-2 truncate text-sm md:text-base">
+    {{ data.curResumeName }}
+    <span v-if="data.curResumeFileName" class="ml-2 text-xs text-light-c">
+      {{ data.curResumeFileName }}
+    </span>
+  </div>
 </template>
 
 <script lang="ts" setup>
